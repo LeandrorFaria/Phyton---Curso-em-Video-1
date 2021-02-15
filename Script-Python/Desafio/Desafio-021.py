7d3e1bbbdba95177df12374dd0fec6378d5bef93
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-15 20:00:04 -0300

    desafio 21

--- Script-Python/Desafio/Desafio-021.py
+++ Script-Python/Desafio/Desafio-021.py
@@ -1,3 +1,7 @@
 # Faça um programa em Python que abra e reproduza um arquivo MP3.
 
-
+import pygame
+pygame.init()
+pygame.mixer.music.load('d-021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
