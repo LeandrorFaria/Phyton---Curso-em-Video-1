e9c5f6a1bcfc622f438b2cd70897b372a0e234e6
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-15 13:39:27 -0300

    desafio 18

--- Script-Python/Desafio/Desafio-017.py
+++ Script-Python/Desafio/Desafio-017.py
@@ -4,3 +4,15 @@ co = float(input('Comprimento do cateto oposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
 h = ((co ** 2) + (ca ** 2)) ** 0.5
 print('A hipotenuza vai medir {:.2f} .'.format(h))
+
+# -- outro modo
+from math import hypot
+hi2 = hypot(co, ca)
+print('A hipotenuza vai medir {:.2f} .'.format(hi2))
+
+# -- outro modo
+import math
+hi = math.hypot(co,ca)
+print('A hipotenuza vai medir {:.2f} .'.format(hi))
+
+
--- Script-Python/Desafio/Desafio-018.py
+++ Script-Python/Desafio/Desafio-018.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+import math
+angulo = float(input('Digite o ângulo que deseja:'))
+seno = math.sin(math.radians(angulo))
+print('O ângulo de {} tem SENO de {:.2f} .'.format(angulo, seno))
+coss = math.cos(math.radians(angulo))
+print('O ângulo de {} tem COSSENO de {:.2f} .' .format(angulo, coss))
+tang = math.tan(math.radians(angulo))
+print('O ângulo de {} tem TANGENTE de {:.2f} .' .format(angulo, tang))
