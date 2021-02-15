cbf1afd71659e8c92235eefd64e2a5441827b129
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-15 13:56:06 -0300

    desafio 20

--- Script-Python/Desafio/Desafio-020.py
+++ Script-Python/Desafio/Desafio-020.py
@@ -1,1 +1,10 @@
 # o mesmo professor do desafio anterior quer sortear a ordem de apresentação dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+import  random
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentação será: ')
+print(lista)
