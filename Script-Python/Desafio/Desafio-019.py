19b1acbdbb3b5e2f077ddc98befd788223e73f77
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-15 13:49:56 -0300

    desafio 19

--- Script-Python/Desafio/Desafio-019.py
+++ Script-Python/Desafio/Desafio-019.py
@@ -1,1 +1,9 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+import random
+a1 = input('Nome do primeiro aluno: ')
+a2 = input('Nome do segundo aluno: ')
+a3 = input('Nome do terceiro aluno: ')
+a4 = input('Nome do quarto aluno: ')
+lista = [a1, a2, a3, a4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi {} .'.format(escolhido))
