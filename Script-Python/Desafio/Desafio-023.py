b04a456a9d08c250b4849f74e12fd662362c61bf
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-21 11:28:30 -0300

    desafio 23

--- Script-Python/Desafio/Desafio-023.py
+++ Script-Python/Desafio/Desafio-023.py
@@ -6,5 +6,12 @@
 # milhar:  1
 
 numero = int(input('Digite um número entre 0 e 9999:'))
-numero_t = numero.split()
-print('unidade',numero_t.)
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+print('Analisando o número {} .' ,format(numero))
+print('A unidade de {} é {} .' .format(numero, u))
+print('A dezena de {} é {} .' .format(numero, d))
+print('A centena de {} é {} .' .format(numero, c))
+print('A milhar de {} é {} .' .format(numero, m))
