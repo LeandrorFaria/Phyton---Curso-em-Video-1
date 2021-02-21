ab5f7b8a7d77b8718119dbc2d1a5efb91c8fddfc
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-21 11:15:59 -0300

    desafio 22

--- Script-Python/Desafio/Desafio-022.py
+++ Script-Python/Desafio/Desafio-022.py
@@ -4,9 +4,11 @@
 # -> Quantas letras ao todos sem considerar espaços
 # -> Quantas letras tem o primeiro nome
 
-nome = input('Digite seu nome completo:')
-nome = nome.strip()
-print(nome.upper())
-print(nome.lower())
-
-
+nome = str(input('Digite seu nome completo:')).strip()
+print('Analisando seu nome ...')
+print('Seu nome em maiúscula é {}'.format(nome.upper()))
+print('Seu nome em minúscula é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras.' .format(len(nome)-nome.count(' ')))
+print('Seu nome tem {} letras.' .format(nome.find(' ')))
+separa = nome.split()
+print('Seu nome {} e tem {} letras.'.format(separa[0], len(separa[0])))
