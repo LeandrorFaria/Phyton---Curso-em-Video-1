5b1568f9ad91d0982c7cea929d198a2c45cb31fb
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-21 11:53:17 -0300

    desafio 26

--- Script-Python/Desafio/Desafio-026.py
+++ Script-Python/Desafio/Desafio-026.py
@@ -2,3 +2,8 @@
 # quantas vezes aparece a letra "A"
 # em que posição ela aparece a primeira vez.
 # em que posição ela aparece da última vez.
+
+frase = str(input('Diga uma frase que te norteia: ')).lower().strip()
+print('A letra a aparece {} vezes.' .format(frase.count('a')))
+print('A primeira letra a apareceu na posição {} .' .format(frase.find('a')+1))
+print('A última letra a apareceu na posição {} .' .format(frase.rfind('a')+1))
