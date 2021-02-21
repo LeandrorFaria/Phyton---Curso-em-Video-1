63960b475c8d3a4b237a5fce219a03f5366dab74
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-21 11:34:13 -0300

    desafio 24

--- Script-Python/Desafio/Desafio-024.py
+++ Script-Python/Desafio/Desafio-024.py
@@ -1,3 +1,7 @@
-# Crie um progrma que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
 
 cidade = input('Digite o nome de uma cidade:')
+cidade = cidade.strip()
+cidade = cidade.upper()
+print(cidade[:5] =='SANTO')
+
