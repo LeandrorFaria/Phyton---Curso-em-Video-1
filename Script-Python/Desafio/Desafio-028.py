93963719cbfd9ab79fde871ad364361ca4fe2280
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-22 08:03:29 -0300

    Aula 10

---
+++ Script-Python/Desafio/Desafio-028.py
@@ -0,0 +1,2 @@
+# Escreva um programa que faça o computador "pensar" em número inteiro entre 0 e 5 peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+# O programa deverá escrever na tela o se o usuário venceu ou perdeu.
---
+++ Script-Python/Desafio/Desafio-030.py
@@ -0,0 +1,1 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ela é Par ou Ímpar.
