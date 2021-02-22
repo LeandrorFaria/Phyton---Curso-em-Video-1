82ed9ab619740d0672a61c3a0b589d619677f8ef
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-22 08:37:38 -0300

    desafio 29

--- Script-Python/Desafio/Desafio-029.py
+++ Script-Python/Desafio/Desafio-029.py
@@ -1,3 +1,13 @@
 # Escreva um programa que leia a velocidade de um carro.
 # se ele ultrapassar 80 km/h, mostre uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$ 7,00 por cada Km acima do limite.
+
+carro = int(input('Qual a velocidade do carro? '))
+print('--=--'*10)
+if carro <= 80:
+    print('Boa viagem.Continue respeitando o limite de velocidade.')
+else:
+    multa = 7 * (carro - 80)
+    print('MULTADO! Você excedeu o limite permitido que é de 80 Km/h.')
+    print('Você deve pagar uma multa de R$ {:.2f}.'.format(multa))
+print('--=--'*10)
