c20f1f1857f790ba1b39f6fce53826c55a94c866
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-23 08:07:31 -0300

    desafio 35

--- Script-Python/Desafio/Desafio-035.py
+++ Script-Python/Desafio/Desafio-035.py
@@ -1,1 +1,11 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print('--=--' * 7)
+print('Analisador de triângulo')
+print('--=--' * 7)
+a = float(input('Primeiro segmento:'))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+if a < b + c and b < a + c and c < a + b:
+    print('Os segmentos informados pode FORMAR um triângulo!')
+else:
+    print('Os segmentos não podem formar um triângulo!')
