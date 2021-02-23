9b15be83acc359b1c8fdcc1ed061e17cad276a6b
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-23 07:57:08 -0300

    desafio 34

--- Script-Python/Desafio/Desafio-034.py
+++ Script-Python/Desafio/Desafio-034.py
@@ -1,3 +1,10 @@
 # Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
 # Para salários superiores a R$ 1.250,00, calcule um aumento de 10%.
 # Para os inferiores ou iguais, o aumento é de 15%.
+salario = float(input('Qual valor atual do salário? '))
+if salario >= 1250:
+    aumento = salario * 0.15
+else :
+    aumento = salario * 0.1
+novo_salario = salario + aumento
+print('O valor do aumento será de R$ {:.2f} e o novo salário ficará em R$ {:.2f}.' .format(aumento, novo_salario))
