f7b3b1cf50bd509e91d7e4cf3c995164e45f61f1
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-23 07:46:38 -0300

    desafio 33

--- Script-Python/Desafio/Desafio-033.py
+++ Script-Python/Desafio/Desafio-033.py
@@ -1,1 +1,17 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+# definindo quem é o menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+# definindo quem é o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor é {} e o maior é {}.' .format(menor, maior))
