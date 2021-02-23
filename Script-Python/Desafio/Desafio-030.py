a716073a90534286078746268066a9d73e76cb8b
LeandrorFaria <leandro.faria@ceodofuturo.org.br>
2021-02-23 07:17:50 -0300

    desafio 30

--- Script-Python/Desafio/Desafio-030.py
+++ Script-Python/Desafio/Desafio-030.py
@@ -1,1 +1,8 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ela é Par ou Ímpar.
+
+numero = int(input('Preencha com um número: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O número {} é PAR.'.format(numero))
+else:
+    print('O número {} é ÍMPAR.' .format(numero))
